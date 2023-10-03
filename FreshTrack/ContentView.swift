39a3b02287abb3288085a24e7d5e184fc9e23026
RCGV1 <Benjaminfaer@gmail.com>
2023-10-02 19:06:45 -0700

    Initial Commit

---
+++ FreshTrack/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  FreshTrack
+//
+//  Created by Benjamin Faershtein on 10/2/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FreshTrack/FreshTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  FreshTrackApp.swift
+//  FreshTrack
+//
+//  Created by Benjamin Faershtein on 10/2/23.
+//
+
+import SwiftUI
+
+@main
+struct FreshTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
